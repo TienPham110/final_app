946713ff4de9b81c940c231638e45236ca0394b6
tienpham110 <phamhongtien19@gmail.com>
2020-08-27 10:53:13 +0700

    create_routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root '/feed', to: 'home#feed'
   get '/discovery', to: 'home#discovery'
-  resources :users, excecpt: [:index, destroy]
+  resources :users, excecpt: [:index, :destroy]
   get '/login', to: 'home#login'
   get '/signup', to: 'home#signup'
    resources :admin do 
