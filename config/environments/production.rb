74e01a586e79ac1aca36710dd7e7218b640f6b36
tienpham110 <phamhongtien19@gmail.com>
2020-09-08 11:01:12 +0700

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.default_url_options = { host: 'https://hidden-earth-68568.herokuapp.com/' }
+  config.action_mailer.perform_deliveries = true
 end
