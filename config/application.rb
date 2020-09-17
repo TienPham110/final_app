14cba361384604309a69f58963e05f4fd9ae584d
tienpham110 <phamhongtien19@gmail.com>
2020-09-17 16:47:44 +0700

    update

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module Blog
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.available_locales = [:en, :vi]
     config.i18n.default_locale = :en
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
