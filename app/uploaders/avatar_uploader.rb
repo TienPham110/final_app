8c7a42670c31cc5181707be864bb00a5d1fd830b
tienpham110 <phamhongtien19@gmail.com>
2020-09-17 16:57:00 +0700

    fix

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -4,11 +4,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.development?
-    storage :file
-  else
-    include Cloudinary::CarrierWave
-  end
+  
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
