0820c3d913fd5bbca8146b83876e4ef32aed8bfe
tienpham110 <phamhongtien19@gmail.com>
2020-09-18 10:41:58 +0700

    fix

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,24 +1,24 @@
-# class NotificationsController < ApplicationController
+class NotificationsController < ApplicationController
     
-#     def index
-#         @notifications = Notification.where(user: current_user).unviewed.leatest
+    def index
+        @notifications = Notification.where(user: current_user).unviewed.leatest
     
-#         respond_to do |format|
-#           format.html
-#           format.js
-#         end
-#     end
+        respond_to do |format|
+          format.html
+          format.js
+        end
+    end
     
-#     def update
-#         @notification = Notification.find(params[:id])
+    def update
+        @notification = Notification.find(params[:id])
     
-#         message = @notification.update(notification_params) ? "Viewed notification" : "There was an error"
+        message = @notification.update(notification_params) ? "Viewed notification" : "There was an error"
     
-#         redirect_to :back, notice: message
-#     end
+        redirect_to :back, notice: message
+    end
     
-#     private
-#         def notification_params
-#             params.require(:notification).permit(:viewed)
-#         end
-# end
+    private
+        def notification_params
+            params.require(:notification).permit(:viewed)
+        end
+end
