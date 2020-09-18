fc6b19b644b49133b9ea093fe2c6fb60269222ee
tienpham110 <phamhongtien19@gmail.com>
2020-09-18 09:59:51 +0700

    fix

--- app/channels/notification_channel.rb
+++ app/channels/notification_channel.rb
@@ -1,10 +1,10 @@
-class NotificationChannel < ApplicationCable::Channel
-  def subscribed
-    # stream_from "some_channel"
-    stream_from "notifications.#{current_user.id}" # in this way we identify to the user inside the channel laters
-  end
+# class NotificationChannel < ApplicationCable::Channel
+#   def subscribed
+#     # stream_from "some_channel"
+#     stream_from "notifications.#{current_user.id}" # in this way we identify to the user inside the channel laters
+#   end
 
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
-end
+#   def unsubscribed
+#     # Any cleanup needed when channel is unsubscribed
+#   end
+# end
